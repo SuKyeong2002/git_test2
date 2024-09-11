3f2270645c23ae89d245758e941f10f3220660bd
Oh, Sukyeong <osukyeong0109@gmail.com>
2024-09-11 10:08:26 +0900

    Hello 클래스에 message 멤버 변수 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,11 +2,8 @@ package git_test2;
 
 public class Hello {
 
-<<<<<<< HEAD
-	String message = "안녕하세요!";
+	String message = "안녕하세요"
 
-=======
->>>>>>> f6ffa9aceb9813728d1dce2792ff54de9cc5e94b
 	public void say() {
 		System.out.println(message);
 	}
